690a9a9b46165c1c13feb56023fbc457699aedad
IsaAliev <isaaliev12@gmail.com>
2017-07-06 19:10:59 +0300

    Initial Commit

---
+++ ios_OneTRAK_test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ios_OneTRAK_test
+//
+//  Created by user on 06.07.17.
+//  Copyright © 2017 ISA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios_OneTRAK_test/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ios_OneTRAK_test
+//
+//  Created by user on 06.07.17.
+//  Copyright © 2017 ISA. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
